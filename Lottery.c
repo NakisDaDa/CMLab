842d3a9fbaca20f5bf9599d427d6c4cd1ca08593
Bcepnez <senju_mr@hotmail.com>
2018-06-25 17:53:00 +0700

    Nakissss

--- Lottery.c
+++ Lottery.c
@@ -2,9 +2,5 @@
 void main()
 {
   
-  printf("Noey BNK48 is the best!");
-  printf("\nNakisDaDa");
-  printf("\nDaDaLand");
-  printf("Mew Mew! is the best too!\n")
-  scanf("%d", &i);
+  printf("Nakis! I want to eat SALMON threat BENZZZZZZZZ");
 }
